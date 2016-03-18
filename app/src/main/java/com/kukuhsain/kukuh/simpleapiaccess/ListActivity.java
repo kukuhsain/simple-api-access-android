89be5a6aa4a3bf07eb19c74117475f4661ea4e5d
Kukuh <kukuhsain@gmail.com>
2016-03-18 15:33:28 +0700

    Add FAB to ListActivity to access AddActivity

--- app/src/main/java/com/kukuhsain/kukuh/simpleapiaccess/ListActivity.java
+++ app/src/main/java/com/kukuhsain/kukuh/simpleapiaccess/ListActivity.java
@@ -1,9 +1,12 @@
 package com.kukuhsain.kukuh.simpleapiaccess;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -18,14 +21,16 @@ public class ListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(ListActivity.this, AddActivity.class);
+                startActivity(intent);
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
             }
-        });*/
+        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
