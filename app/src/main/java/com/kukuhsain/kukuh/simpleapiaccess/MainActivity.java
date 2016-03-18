c11ec6d20c399cc327f4897fa977eed45a7ebfbb
Kukuh <kukuhsain@gmail.com>
2016-03-18 11:26:22 +0700

    Add list activity with its layouts

--- app/src/main/java/com/kukuhsain/kukuh/simpleapiaccess/MainActivity.java
+++ app/src/main/java/com/kukuhsain/kukuh/simpleapiaccess/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kukuhsain.kukuh.simpleapiaccess;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,14 +19,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+    }
+
+    public void goToNext(View view) {
+        Intent intent = new Intent(MainActivity.this, )
     }
 
     @Override
