1412d8e83233cc8118125b4b7223e99c5257ac9a
Kukuh <kukuhsain@gmail.com>
2016-03-18 11:34:24 +0700

    Add some dependencies

--- app/src/main/java/com/kukuhsain/kukuh/simpleapiaccess/MainActivity.java
+++ app/src/main/java/com/kukuhsain/kukuh/simpleapiaccess/MainActivity.java
@@ -2,13 +2,11 @@ package com.kukuhsain.kukuh.simpleapiaccess;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToNext(View view) {
-        Intent intent = new Intent(MainActivity.this, )
+        Intent intent = new Intent(MainActivity.this, ListActivity.class);
+        startActivity(intent);
     }
 
     @Override
---
+++ app/src/main/java/com/kukuhsain/kukuh/simpleapiaccess/Review.java
@@ -0,0 +1,7 @@
+package com.kukuhsain.kukuh.simpleapiaccess;
+
+/**
+ * Created by kukuh on 18/03/16.
+ */
+public class Review {
+}
